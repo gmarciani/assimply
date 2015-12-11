9cc9881f47a5cb3b56a62f045740e9bb1f38b228
Giacomo Marciani <giacomo.marciani@gmail.com>
2015-12-11 17:23:29 +0100

    Simple refactoring.

--- math/sum-two-numbers.asm
+++
@@ -1,9 +0,0 @@
-# The program computes the sum of two numbers stored in registers
-
-.text
-.globl  main
-
-main:
-	ori $8, $0, 0xA # loads "10" into register 8
-	ori $9, $0, 0xC # loads "12" into register 9
-	add $10,$8, $9  # adds registers 8 and 9, then puts the result in register 10
